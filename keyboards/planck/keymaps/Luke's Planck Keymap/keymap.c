d5254c7052fbba4a7003ba6a01ee1be813287870
lctoe <68288189+lctoe@users.noreply.github.com>
2020-09-03 21:49:50 -0400

    9-3-20

--- keyboards/planck/keymaps/Luke's Planck Keymap/keymap.c
+++ keyboards/planck/keymaps/Luke's Planck Keymap/keymap.c
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_L0] = LAYOUT_planck_grid(
     KC_ESC,            KC_QUOT, KC_COMM, KC_DOT,  KC_P,        KC_Y,    KC_F,    KC_G,       KC_C,    KC_R,    KC_L,    KC_SLSH,
-    KC_MUTE,           KC_A,    KC_O,    KC_E,    KC_U,        KC_I,    KC_D,    KC_H,       KC_T,    KC_N,    KC_S,    KC_MINS,
-    KC_LSFT,           KC_SCLN, KC_Q,    KC_J,    KC_K,        KC_X,    KC_B,    KC_M,       KC_W,    KC_V,    KC_Z,    KC_ENT ,
+    KC_MUTE,           KC_A,    KC_O,    KC_E,    KC_U,        KC_I,    KC_D,    KC_H,       KC_T,    KC_N,    KC_S,    KC_ENT ,
+    KC_LSFT,           KC_SCLN, KC_Q,    KC_J,    KC_K,        KC_X,    KC_B,    KC_M,       KC_W,    KC_V,    KC_Z,    KC_MINS,
     LM(_Ctrl, MOD_CTL),KC_LGUI, KC_LALT, KC_TAB , MO(_Lower),  KC_BSPC, KC_SPC,  MO(_Raise), KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
@@ -147,8 +147,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,                 KC_T,    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
-    _______, KC_A,    KC_S,    KC_D,    KC_F,                 KC_G,    KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,    KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    _______, KC_A,    KC_S,    KC_D,    KC_F,                 KC_G,    KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_ENT ,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,    KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
     _______, KC_LGUI, KC_LALT, KC_TAB,  LT(_Lower, KC_SPC),   KC_MINS, KC_SPC,  MO(_Raise), KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT
 )
 };
