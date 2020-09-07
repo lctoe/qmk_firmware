827c4ac1a031827f661e08538458f2afe393232a
lctoe <68288189+lctoe@users.noreply.github.com>
2020-09-07 11:44:36 -0400

    New 9-7

--- keyboards/planck/keymaps/luke/keymap.c
+++ keyboards/planck/keymaps/luke/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MPLY,            KC_QUOT, KC_COMM, KC_DOT,      KC_P,        KC_Y,    KC_F,    KC_G,       KC_C,        KC_R,    KC_L,    KC_SLSH,
     KC_ESC,             KC_A,    KC_O,    KC_E,        KC_U,        KC_I,    KC_D,    KC_H,       KC_T,        KC_N,    KC_S,    KC_ENT ,
     KC_LSFT,            KC_SCLN, KC_Q,    KC_J,        KC_K,        KC_X,    KC_B,    KC_M,       KC_W,        KC_V,    KC_Z,    KC_MINS,
-    LM(_Ctrl, MOD_LCTL),KC_LGUI, KC_LALT, TT(_QWERTY), MO(_Lower),  KC_BSPC, KC_SPC,  MO(_Raise), TT(_Number), KC_LEFT, KC_DOWN, KC_RGHT
+    LM(_Ctrl, MOD_LCTL),KC_LGUI, KC_LALT, TG(_QWERTY), MO(_Lower),  KC_BSPC, KC_SPC,  MO(_Raise), TT(_Number), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Raise
